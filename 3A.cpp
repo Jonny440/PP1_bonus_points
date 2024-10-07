bc5cf05fd28b541aa42734f2b5ea0f259778635e
Jonny440 <zh.batyrbekov1204@gmail.com>
2024-10-07 15:40:33 +0500

    Create 3A.cpp

---
+++ 3A.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+    for(int i = x; i <= y; i++){
+        if (i % 2 == 0){
+            cout << i << " ";
+        }
+    }
+    return 0;
+}
